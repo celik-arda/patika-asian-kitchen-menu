990f5c5b2ecbaa67e6d79cd72d042bc724a312de
Arda Celik <ardacelik.dev@gmail.com>
2022-04-07 23:08:38 +0300

    [commit -2]  project.js dosyasinda butun menudeki yemeklerin hepsi, hicbir siralama yapmadan gosterilecek ve bootstrap-css ozellikleriyle uyumlu olacak sekilde forEach ve Dom manipulasyonu araciligiyla sayfaya yerlestirildi.  Bu gorunum, butonlar eklendikten sonra sayfanin default gorunumu olacak. (category:all).  Sonraki commitlerde buton yapisi olusturulacak ve kategoriler icin fonksiyonlar modullere ayrilmaya calisilacak.

--- project.js
+++ project.js
@@ -1,12 +1,36 @@
-//  MAIN PROJECT JS DOCUMENT   //
+            //  MAIN PROJECT JS DOCUMENT   //
 
 
 
 const menuItemsField = document.querySelector(".section-center");
 
+const writeTheMenuItems = () => {
 
+    allMenuItems = "";
 
+    menu.forEach((e) => {
 
+        allMenuItems += `<div class="menu-items col-lg-6 col-sm-12">
+        <img src="${e.img}" alt="${e.title}" class="photo">
+        <div class="menu-info">
+            <div class="menu-title">
+                <h4>
+                    ${e.title}
+                </h4>
+                <h4>
+                    ${e.price}
+                </h4>
+            </div>
+            <div class="menu-text">
+                ${e.desc}
+            </div>
+        </div>
+      </div>`;
+    });
 
+    menuItemsField.innerHTML = allMenuItems;
+}
 
 writeTheMenuItems();
+
+
