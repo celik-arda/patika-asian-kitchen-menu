f6fe96da9a5e899d1f695ac7c12a9c03e8ecace0
Arda Celik <ardacelik.dev@gmail.com>
2022-04-08 15:42:31 +0300

    [commit -3]  project.js'de yemek kategorileri icin ayri bir array olusturuldu. Amac kategorileri ayri bir modul gibi yazarak daha rahat degistirilecek-revize edilebilecek sekilde olmasi.  Array'den gelen bu kategorilerle sayfadaki butonlar ve bunlarin id degerleri belirlendi.  Sonraki adimda muhtemelen butonlara eventler atanacak ve butonlarin ayri ayri siralama-filtreleme yapmasi amaclanacak.

--- project.js
+++ project.js
@@ -1,9 +1,43 @@
             //  MAIN PROJECT JS DOCUMENT   //
 
 
-
+// class isimleri nasılsa birden fazla bulunmadığı için HTML Token'a oranla daha fazla kolaylık sağlasın diye querySelector'ı kullanıyorum//
 const menuItemsField = document.querySelector(".section-center");
+const buttonsField = document.querySelector(".btn-container");
 
+
+
+//  !!!  Burada butonları doğrudan fonksiyonun içine yazıyla eklemek daha basit olacaktı ancak projede bu tarz kategori objeleri sonradan eklenebilir ya da revize edilebilir. Bunun daha kolay olması için ayrı bir array olarak oluşturup buton isimlerini bu array'in içinden gönderiyorum.  //
+const menuCategories = [
+
+    "All",
+    "Korea",
+    "Japan",
+    "China"
+];
+
+
+//  While Page Is Openning, Create The Category Buttons In UI //
+const createTheAllButtons = () => {
+    
+    myAllButtons = "";
+
+    menuCategories.forEach((e) => {
+
+        myAllButtons += `<button class="btn btn-item btn-outline-success" id="${e}">
+                            ${e}
+                        </button>`;
+
+    });
+
+    buttonsField.innerHTML = myAllButtons;
+}
+
+createTheAllButtons();
+
+
+
+// Display All Menu Items As A Default //
 const writeTheMenuItems = () => {
 
     allMenuItems = "";
@@ -32,5 +66,4 @@ const writeTheMenuItems = () => {
 }
 
 writeTheMenuItems();
-
 
