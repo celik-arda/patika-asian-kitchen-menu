e861186ba0dd9a8ba2c84dd8491e944a2a0a3e7a
Arda Celik <ardacelik.dev@gmail.com>
2022-04-19 23:43:58 +0300

    [commit -4]  default all butonunun butun menu itemlerini yazdirmasi icin forEach kullanilmisti.  Simdi ise tiklanan buton tipine gore ayri ayri elemanlarin secilip yazilmasi icin map ve filter metodlari kullanildi. Console.log ile deneme yapildi, buraya kadar sorun yok. Bundan sonrasinda kategorilere gore secilen menu itemleri butun itemleri default olarak yazdirmami saglayan forEach fonksiyonuma gonderilecek.

--- project.js
+++ project.js
@@ -1,4 +1,4 @@
-            //  MAIN PROJECT JS DOCUMENT   //
+//  MAIN PROJECT JS DOCUMENT   //
 
 
 // class isimleri nasılsa birden fazla bulunmadığı için HTML Token'a oranla daha fazla kolaylık sağlasın diye querySelector'ı kullanıyorum//
@@ -9,7 +9,7 @@ const buttonsField = document.querySelector(".btn-container");
 
 //  !!!  Burada butonları doğrudan fonksiyonun içine yazıyla eklemek daha basit olacaktı ancak projede bu tarz kategori objeleri sonradan eklenebilir ya da revize edilebilir. Bunun daha kolay olması için ayrı bir array olarak oluşturup buton isimlerini bu array'in içinden gönderiyorum.  //
 const menuCategories = [
-
+    
     "All",
     "Korea",
     "Japan",
@@ -21,29 +21,82 @@ const menuCategories = [
 const createTheAllButtons = () => {
     
     myAllButtons = "";
-
+    
     menuCategories.forEach((e) => {
-
+        
         myAllButtons += `<button class="btn btn-item btn-outline-success" id="${e}">
-                            ${e}
-                        </button>`;
-
+        ${e}
+        </button>`;
+        
     });
-
+    
     buttonsField.innerHTML = myAllButtons;
 }
 
 createTheAllButtons();
 
 
+const allButtons = document.querySelectorAll(".btn-item");
+
+
+// Control The All Event Listeners In A Single Function //
+const allEventListeners = () => {
+    
+    buttonsField.addEventListener("click",displayMenuItemsByCategory);
+}
+
+
+// Display The Menu Items in Terms of Category Buttons //
+const displayMenuItemsByCategory = (e) => {
+    
+    const clickedCategory = e.target;
+
+    if(clickedCategory.id === "All"){
+
+        // Site Başlangıcındaki default "all" gösterimi için forEach kullanıyorum.
+        writeTheMenuItems();
+    }
+    // Diğer bütün kategorileri aşağıda tek bir map döngüsü fonksiyonuna yolluyorum.
+    else{
+        selectCategoryBeforeDisplay(clickedCategory);
+    }
+}
+
+// If Clicking a Category Button ( Except "All" ), Select and Show The Menu Items //
+const selectCategoryBeforeDisplay = (clickedCategory) => {
+
+    const clickedButtonId = clickedCategory.id;
+
+    const menuDeneme = menu.filter((menuItem) => {
+
+        return menuItem.category === clickedButtonId
+
+    }).map((e) => {
+
+        return {
+
+            id: e.id,
+            img: e.img,
+            title: e.title,
+            price: e.price,
+            desc: e.desc,
+        }
+    });
+
+    console.log(menuDeneme);
+}
+
+
+allEventListeners();
+
 
 // Display All Menu Items As A Default //
 const writeTheMenuItems = () => {
 
     allMenuItems = "";
-
+    
     menu.forEach((e) => {
-
+        
         allMenuItems += `<div class="menu-items col-lg-6 col-sm-12">
         <img src="${e.img}" alt="${e.title}" class="photo">
         <div class="menu-info">
